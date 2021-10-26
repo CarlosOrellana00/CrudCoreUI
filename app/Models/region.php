d1cd334166eae138cad82678fce260c03f1be6e0
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2021-10-25 22:58:42 -0400

    modelos actualizados

--- app/Models/region.php
+++ app/Models/region.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class region extends Model
 {
     use HasFactory;
+
+    protected $table = "region";
+    public $timestamps =  true;
+
+    protected $fillable = [
+      'nombre',
+    ];
 }
--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Region;
 
 class usuario extends Model
 {
     use HasFactory;
+
+    protected $table = 'usuario';
+    public $timestamps = true;
+
+    protected $fillable = [
+      'rut',
+      'nombre',
+      'apellido',
+      'correo',
+      'id_region'
+    ];
+
+    public function region(){
+      return $this->belongsTo(Region::class,'id_region');
+    }
 }
